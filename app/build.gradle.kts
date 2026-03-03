a75e1a82a099ed874c4a6c46c7ddbb59ae7583f6
Yani Meziani <mezianiyani0@gmail.com>
2026-03-03 12:17:47 +0000

    Bump version for API updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "org.dragun.pegasus"
         minSdk = 28
         targetSdk = 36
-        versionCode = 7
-        versionName = "0.2.5"
+        versionCode = 8
+        versionName = "0.2.6"
 
         ndk {
             abiFilters += "arm64-v8a"
